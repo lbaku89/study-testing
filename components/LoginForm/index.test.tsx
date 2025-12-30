e97bdf68dbe17bb439643c4205d957531b8be643
hyunwoo <khj930410@naver.com>
2025-12-30 16:47:25 +0900

    feat: 컴포넌트에서 특정 부분 가져오는 코드

---
+++ components/LoginForm/index.test.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import LoginForm from "./index";
+
+test("로그인 폼이 올바르게 렌더링 되는지 확인", () => {
+  render(<LoginForm />);
+
+  // 라벨로 입력(Input) 요소 찾기
+  const emailByLabel = screen.getByLabelText("이메일:");
+
+  // 플레이스 홀더로 입력 요소 찾기
+  const emailByPlaceholder = screen.getByPlaceholderText("이메일을 입력하세요");
+
+  // 역할로 버튼 찾기
+  const emailByRole = screen.getByRole("textbox");
+
+  // 테스트 ID로 요소 찾기
+  const emailByTestId = screen.getByTestId("email-input");
+
+  // 모든 요소가 화면에 있는지 확인
+  expect(emailByLabel).toBeInTheDocument();
+  expect(emailByPlaceholder).toBeInTheDocument();
+  expect(emailByRole).toBeInTheDocument();
+  expect(emailByTestId).toBeInTheDocument();
+
+  // emailByLabel, emailByPlaceholder, emailByTestId가 동일한 요소인지 확인
+  expect(emailByLabel).toBe(emailByPlaceholder);
+  expect(emailByLabel).toBe(emailByTestId);
+});
