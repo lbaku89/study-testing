ba0e471593804a7d44617435c2eb35b36d1d4dc8
hyunwoo <khj930410@naver.com>
2025-12-30 23:55:09 +0900

    refactor: test 구조화 및 반복되는 코드 제거

--- components/LoginForm/index.test.tsx
+++ components/LoginForm/index.test.tsx
@@ -1,86 +1,69 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { LoginForm } from "./index";
 
-beforeEach(() => {
-  // 모든 테스트 전에 LoginForm 컴포넌트를 렌더링합니다.
-  render(<LoginForm />);
-});
+describe("로그인 폼 렌더링 테스트", () => {
+  let loginButton: HTMLButtonElement,
+    emailInput: HTMLInputElement,
+    passwordInput: HTMLInputElement;
 
-test("로그인 폼이 올바르게 렌더링 되는지 확인", () => {
-  // 라벨로 입력(Input) 요소 찾기
-  const emailByLabel = screen.getByLabelText("이메일");
+  beforeEach(() => {
+    // 모든 테스트 전에 LoginForm 컴포넌트를 렌더링합니다.
+    render(<LoginForm />);
+    loginButton = screen.getByRole("button", { name: "로그인" });
+    emailInput = screen.getByLabelText("이메일");
+    passwordInput = screen.getByLabelText("비밀번호");
+  });
 
-  // 플레이스 홀더로 입력 요소 찾기
-  const emailByPlaceholder = screen.getByPlaceholderText("이메일을 입력하세요");
+  describe("버튼 활성화 상태", () => {
+    test("로그인 폼의 이메일과 비밀번호 미입력 시 로그인 버튼 비활성화되는지 확인", () => {
+      // 입력 필드 값이 비어있는지 확인
+      expect(emailInput).toHaveValue("");
+      expect(passwordInput).toHaveValue("");
 
-  // 역할로 버튼 찾기
-  const emailByRole = screen.getByRole("textbox");
+      // 로그인 버튼이 비활성화되어 있는지 확인
+      expect(loginButton).toBeDisabled();
+    });
+    test("이메일, 비밀번호 입력 시 로그인 버튼 활성화되는지 확인", () => {
+      // 이메일과 비밀번호 입력
+      fireEvent.change(emailInput, { target: { value: "test@test.com" } });
+      fireEvent.change(passwordInput, { target: { value: "test" } });
 
-  // 모든 요소가 화면에 있는지 확인
-  expect(emailByLabel).toBeInTheDocument();
-  expect(emailByPlaceholder).toBeInTheDocument();
-  expect(emailByRole).toBeInTheDocument();
-  expect(emailByLabel).toBe(emailByPlaceholder);
-});
+      // 로그인 버튼이 활성화되어 있는지 확인
+      expect(loginButton).toBeEnabled();
+    });
+  });
 
-test("로그인 폼의 이메일과 비밀번호 미입력 시 로그인 버튼 비활성화되는지 확인", () => {
-  const emailInput = screen.getByLabelText("이메일");
-  const passwordInput = screen.getByLabelText("비밀번호");
-  const loginButton = screen.getByRole("button", { name: "로그인" });
+  describe("유효성 검사", () => {
+    test("이메일 잘못 입력 시 에러 메시지 표시", () => {
+      // 잘못된 이메일 입력
+      fireEvent.change(emailInput, { target: { value: "invalid-email" } });
 
-  expect(emailInput).toHaveValue("");
-  expect(passwordInput).toHaveValue("");
-  // expect(loginButton).toBeDisabled()
+      // 에러 메시지 확인
+      const emailErrorMessage =
+        screen.getByText("올바른 이메일 형식이 아닙니다.");
+      expect(emailErrorMessage).toBeInTheDocument();
+    });
+    test("비밀번호 6자 미만 입력 시 에러 메시지 표시", () => {
+      // 잘못된 비밀번호 입력
+      fireEvent.change(passwordInput, { target: { value: "12345" } });
 
-  expect(loginButton).toBeDisabled();
-});
+      // 에러 메시지 확인
+      const passwordErrorMessage = screen.getByText(
+        "비밀번호는 6자 이상이어야 합니다."
+      );
+      expect(passwordErrorMessage).toBeInTheDocument();
+    });
+    test("제대로 된 이메일 입력 시 에러 메세지 사라짐", () => {
+      // 1. 잘못된 이메일 입력 후 에러 메세지 나오는지 확인
+      fireEvent.change(emailInput, { target: { value: "invalid-email" } });
+      const errorMessage = screen.getByText("올바른 이메일 형식이 아닙니다.");
+      expect(errorMessage).toBeInTheDocument();
 
-test("이메일, 비밀번호 입력 시 로그인 버튼 활성화되는지 확인", () => {
-  const emailInput = screen.getByLabelText("이메일");
-  const passwordInput = screen.getByLabelText("비밀번호");
-  const loginButton = screen.getByRole("button", { name: "로그인" });
+      // 2. 올바른 이메일 입력 후 에러 메세지 사라지는지 확인
+      fireEvent.change(emailInput, { target: { value: "test@test.com" } });
+      expect(errorMessage).not.toBeInTheDocument();
 
-  // 이메일과 비밀번호 입력
-  fireEvent.change(emailInput, { target: { value: "test@test.com" } });
-  fireEvent.change(passwordInput, { target: { value: "test" } });
-
-  // 로그인 버튼이 활성화되었는지 확인
-  expect(loginButton).toBeEnabled();
-});
-
-test('이메일 잘못 입력 시 "올바른 이메일 형식이 아닙니다." 라는 에러 메시지가 표시되는지 확인', () => {
-  const emailInput = screen.getByLabelText("이메일");
-
-  // 잘못된 이메일 입력
-  fireEvent.change(emailInput, { target: { value: "invalid-email" } });
-
-  // 에러 메시지 확인
-  const emailErrorMessage = screen.getByText("올바른 이메일 형식이 아닙니다.");
-  expect(emailErrorMessage).toBeInTheDocument();
-});
-
-test('비밀번호 6자 미만 입력 시 "비밀번호는 6자 이상이어야 합니다." 라는 에러 메시지가 표시되는지 확인', () => {
-  const passwordInput = screen.getByLabelText("비밀번호");
-
-  // 잘못된 비밀번호 입력
-  fireEvent.change(passwordInput, { target: { value: "12345" } });
-
-  // 에러 메시지 확인
-  const passwordErrorMessage = screen.getByText(
-    "비밀번호는 6자 이상이어야 합니다."
-  );
-  expect(passwordErrorMessage).toBeInTheDocument();
-});
-
-test("제대로 된 이메일 입력 시 에러 메세지가 사라지는 확인", () => {
-  const emailInput = screen.getByLabelText("이메일");
-
-  // 1. 잘못된 이메일 입력 후 에러 메세지 나오는지 확인
-  fireEvent.change(emailInput, { target: { value: "invalid-email" } });
-  const errorMessage = screen.getByText("올바른 이메일 형식이 아닙니다.");
-  expect(errorMessage).toBeInTheDocument();
-
-  // 2. 올바른 이메일 입력 후 에러 메세지 사라지는지 확인
-  fireEvent.change(emailInput, { target: { value: "test@test.com" } });
-  expect(errorMessage).not.toBeInTheDocument();
+      /* ... */
+    });
+  });
 });
