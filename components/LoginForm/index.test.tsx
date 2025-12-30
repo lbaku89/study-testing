f50162ad1aee8273f39e713ad32d67b7a1117198
hyunwoo <khj930410@naver.com>
2025-12-30 23:23:58 +0900

    refactor: beforeEach 이용 하여 중복 코드 단일코드로 리팩토링

--- components/LoginForm/index.test.tsx
+++ components/LoginForm/index.test.tsx
@@ -1,9 +1,12 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { LoginForm } from "./index";
 
-test("로그인 폼이 올바르게 렌더링 되는지 확인", () => {
+beforeEach(() => {
+  // 모든 테스트 전에 LoginForm 컴포넌트를 렌더링합니다.
   render(<LoginForm />);
+});
 
+test("로그인 폼이 올바르게 렌더링 되는지 확인", () => {
   // 라벨로 입력(Input) 요소 찾기
   const emailByLabel = screen.getByLabelText("이메일");
 
@@ -13,23 +16,14 @@ test("로그인 폼이 올바르게 렌더링 되는지 확인", () => {
   // 역할로 버튼 찾기
   const emailByRole = screen.getByRole("textbox");
 
-  // 테스트 ID로 요소 찾기
-  // const emailByTestId = screen.getByTestId("email-input");
-
   // 모든 요소가 화면에 있는지 확인
   expect(emailByLabel).toBeInTheDocument();
   expect(emailByPlaceholder).toBeInTheDocument();
   expect(emailByRole).toBeInTheDocument();
-  // expect(emailByTestId).toBeInTheDocument();
-
-  // emailByLabel, emailByPlaceholder, emailByTestId가 동일한 요소인지 확인
   expect(emailByLabel).toBe(emailByPlaceholder);
-  // expect(emailByLabel).toBe(emailByTestId);
 });
 
 test("로그인 폼의 이메일과 비밀번호 미입력 시 로그인 버튼 비활성화되는지 확인", () => {
-  render(<LoginForm />);
-
   const emailInput = screen.getByLabelText("이메일");
   const passwordInput = screen.getByLabelText("비밀번호");
   const loginButton = screen.getByRole("button", { name: "로그인" });
@@ -42,8 +36,6 @@ test("로그인 폼의 이메일과 비밀번호 미입력 시 로그인 버튼 
 });
 
 test("이메일, 비밀번호 입력 시 로그인 버튼 활성화되는지 확인", () => {
-  render(<LoginForm />);
-
   const emailInput = screen.getByLabelText("이메일");
   const passwordInput = screen.getByLabelText("비밀번호");
   const loginButton = screen.getByRole("button", { name: "로그인" });
@@ -57,8 +49,6 @@ test("이메일, 비밀번호 입력 시 로그인 버튼 활성화되는지 확
 });
 
 test('이메일 잘못 입력 시 "올바른 이메일 형식이 아닙니다." 라는 에러 메시지가 표시되는지 확인', () => {
-  render(<LoginForm />);
-
   const emailInput = screen.getByLabelText("이메일");
 
   // 잘못된 이메일 입력
@@ -70,8 +60,6 @@ test('이메일 잘못 입력 시 "올바른 이메일 형식이 아닙니다." 
 });
 
 test('비밀번호 6자 미만 입력 시 "비밀번호는 6자 이상이어야 합니다." 라는 에러 메시지가 표시되는지 확인', () => {
-  render(<LoginForm />);
-
   const passwordInput = screen.getByLabelText("비밀번호");
 
   // 잘못된 비밀번호 입력
@@ -85,10 +73,7 @@ test('비밀번호 6자 미만 입력 시 "비밀번호는 6자 이상이어야 
 });
 
 test("제대로 된 이메일 입력 시 에러 메세지가 사라지는 확인", () => {
-  render(<LoginForm />);
-
   const emailInput = screen.getByLabelText("이메일");
-  // const passwordInput = screen.getByLabelText("비밀번호");
 
   // 1. 잘못된 이메일 입력 후 에러 메세지 나오는지 확인
   fireEvent.change(emailInput, { target: { value: "invalid-email" } });
